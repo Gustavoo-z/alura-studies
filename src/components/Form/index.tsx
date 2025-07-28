b912b0e84072d8c3883e0c287cdc035cb25fd189
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-07-28 10:58:07 -0300

    Review React state and controlled components

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
 import Button from "../Button";
 import style from "./Form.module.scss";
 
 export default function Form() {
+  const [tempo, setTempo] = useState("00:00:00");
+  const [tarefa, setTarefa] = useState("");
+
+  function adicionarTarefa() {
+    console.log({
+      tarefa,
+      tempo,
+    })
+    setTarefa("");
+    setTempo("00:00:00");
+  }
+
   return (
-    <form className={style.novaTarefa}>
+    <form className={style.novaTarefa} onSubmit={(e) => {
+      e.preventDefault();
+      adicionarTarefa();
+    }}>
       <div className={style.inputContainer}>
         <label htmlFor="tarefa">Adicione um novo estudo</label>
         <input
           type="text"
           name="tarefa"
           id="tarefa"
+          value={tarefa}
+          onChange={(e) => setTarefa(e.target.value)}
           placeholder="O que você quer estudar?"
           required
         />
@@ -21,6 +39,8 @@ export default function Form() {
           name="tempo"
           id="tempo"
           step="1"
+          value={tempo}
+          onChange={(e) => setTempo(e.target.value)}
           min="00:00:00"
           max="01:30:00"
           required
